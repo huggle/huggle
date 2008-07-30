0b8e7fe2aabc774a94ab0f26967a4e8cabf6784c
adamshorland <adamshorland@a312afe4-d050-0410-810d-3752ba903f19>
2008-07-30 19:46:35 +0000

    Refix (r148) as that fix didn't quite work

--- Forms/StatsForm.Designer.vb
+++ Forms/StatsForm.Designer.vb
@@ -39,7 +39,7 @@ Partial Class StatsForm
         'CloseButton
         '
         Me.CloseButton.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.CloseButton.Location = New System.Drawing.Point(158, 110)
+        Me.CloseButton.Location = New System.Drawing.Point(167, 118)
         Me.CloseButton.Name = "CloseButton"
         Me.CloseButton.Size = New System.Drawing.Size(75, 23)
         Me.CloseButton.TabIndex = 0
@@ -72,7 +72,7 @@ Partial Class StatsForm
         Me.Actions.Location = New System.Drawing.Point(12, 25)
         Me.Actions.MultiSelect = False
         Me.Actions.Name = "Actions"
-        Me.Actions.Size = New System.Drawing.Size(221, 79)
+        Me.Actions.Size = New System.Drawing.Size(230, 87)
         Me.Actions.TabIndex = 2
         Me.Actions.UseCompatibleStateImageBehavior = False
         Me.Actions.View = System.Windows.Forms.View.Details
@@ -96,7 +96,7 @@ Partial Class StatsForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(245, 145)
+        Me.ClientSize = New System.Drawing.Size(254, 153)
         Me.Controls.Add(Me.Actions)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.CloseButton)
--- Forms/StatsForm.vb
+++ Forms/StatsForm.vb
@@ -34,4 +34,7 @@ Class StatsForm
         Close()
     End Sub
 
+    Private Sub StatsForm_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
 End Class
