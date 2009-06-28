cf9136f795cf96efcb7b7848025f47b9907ec3d7
hugglegurch <hugglegurch@a312afe4-d050-0410-810d-3752ba903f19>
2009-06-28 15:31:51 +0000

    bump version to 0.9.2

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -8,5 +8,5 @@ Imports System.Runtime.InteropServices
 <Assembly: AssemblyCopyright("")> 
 <Assembly: AssemblyTrademark("")> 
 <Assembly: ComVisible(False)> 
-<Assembly: AssemblyVersion("0.9.1")> 
-<Assembly: AssemblyFileVersion("0.9.1")> 
+<Assembly: AssemblyVersion("0.9.2")> 
+<Assembly: AssemblyFileVersion("0.9.2")> 
