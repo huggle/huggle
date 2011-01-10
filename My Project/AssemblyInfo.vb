6b21c23c63a8a5b172491f7c28eaf34a8ca09e7d
adamshorland@gmail.com <adamshorland@gmail.com@a312afe4-d050-0410-810d-3752ba903f19>
2011-01-10 19:56:59 +0000

    Bump new version to 1.0.0 (Merged release)

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -8,5 +8,5 @@ Imports System.Runtime.InteropServices
 <Assembly: AssemblyCopyright("")> 
 <Assembly: AssemblyTrademark("")> 
 <Assembly: ComVisible(False)> 
-<Assembly: AssemblyVersion("0.9.11")> 
-<Assembly: AssemblyFileVersion("0.9.11")> 
+<Assembly: AssemblyVersion("1.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0")> 
