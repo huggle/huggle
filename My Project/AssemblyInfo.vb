5d23416bae3cc9820d309646eeb90f3444a89eaa
adamshorland@gmail.com <adamshorland@gmail.com@a312afe4-d050-0410-810d-3752ba903f19>
2011-01-16 11:23:38 +0000

    Version number to 2.1.2

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -8,5 +8,5 @@ Imports System.Runtime.InteropServices
 <Assembly: AssemblyCopyright("")> 
 <Assembly: AssemblyTrademark("")> 
 <Assembly: ComVisible(False)> 
-<Assembly: AssemblyVersion("2.1.1")> 
-<Assembly: AssemblyFileVersion("2.1.1")> 
+<Assembly: AssemblyVersion("2.1.2")> 
+<Assembly: AssemblyFileVersion("2.1.2")> 
