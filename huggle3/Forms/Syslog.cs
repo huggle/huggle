e0df21c1545d1cf34958946e3a2b0dfe54cd379b
Petr Bena <benapetr@gmail.com>
2013-01-19 10:02:39 +0100

    forgot to insert new files

---
+++ huggle3/Forms/Syslog.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace huggle3.Forms
+{
+    public partial class Syslog : Form
+    {
+        public Syslog()
+        {
+            InitializeComponent();
+        }
+
+        private void Syslog_Load(object sender, EventArgs e)
+        {
+            lock (Core.SystemLog)
+            {
+                textBox1.Lines = Core.SystemLog.ToArray();
+            }
+        }
+
+        private void timer0_Tick(object sender, EventArgs e)
+        {
+            lock (Core.SystemLog)
+            {
+                textBox1.Lines = Core.SystemLog.ToArray();
+            }
+        }
+    }
+}
