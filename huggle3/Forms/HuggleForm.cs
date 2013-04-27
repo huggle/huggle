079859465921a9e081b230faa7f5ecb00b418d58
Petr Bena <benapetr@gmail.com>
2013-04-27 14:55:01 +0200

    created initial main form

---
+++ huggle3/Forms/HuggleForm.cs
@@ -0,0 +1,33 @@
+//This is a source code or part of Huggle project
+//
+//This file contains code for form
+
+/// <DOCUMENTATION>
+/// This is a primary form, it contains most of stuff you need
+/// </DOCUMENTATION>
+
+//Copyright (C) 2011-2012 Huggle team
+
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+
+using System;
+
+namespace huggle3.Forms
+{
+	public partial class HuggleForm : Gtk.Window
+	{
+		public HuggleForm () : 	base(Gtk.WindowType.Toplevel)
+		{
+			this.Build ();
+		}
+	}
+}
+
