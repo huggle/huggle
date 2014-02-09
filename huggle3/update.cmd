4885540ad47539758fe9502f7af45c46e474e9e2
addshore <addshorewiki@gmail.com>
2014-02-09 16:20:39 +0100

    Split huggle3 into its own repo
    see https://github.com/huggle/huggle-Csharp

--- huggle3/update.cmd
+++
@@ -1,8 +0,0 @@
-if defined ProgramFiles(x86) (
-PATH=%PATH%;%ProgramFiles(x86)%\Git\bin
-) else (
-PATH=%PATH%;%ProgramFiles%\Git\bin
-)
-cd %1
-git rev-list HEAD --count > %2
-git describe --always >> %2
--- huggle3/update.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-git rev-list HEAD --count > version.txt
-git describe --always >> version.txt
